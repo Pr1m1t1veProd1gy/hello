671eaa9a198aeb0698950caae84c92c04ca83195
Jacob Mir <jacobmir3@gmail.com>
2019-01-17 12:56:09 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jacob Mir
+ * Date: 01/15/2019
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
